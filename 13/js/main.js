c1b4370c7f267a4015dafaf2423010894c8b7dc5
Keks <keks@htmlacademy.ru>
2026-08-19 17:11:43 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,12 @@
+
+import './picture.js';
+
+
+import './picture.js';
+
+
+import { ALL_PHOTOS } from './constants.js';
+import { photoDescription } from './data.js';
+
+Array.from({ length: ALL_PHOTOS }, photoDescription);
+
